6cebde90e3f3cfb1e11b604c497aa3ee844704f1
krishna lal <krishna.lal@synechron.com>
2020-09-13 23:37:39 +0530

    data made dynamic through database

--- listings/views.py
+++ listings/views.py
@@ -1,15 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from .models import Listing
+from django.core.paginator import Paginator, EmptyPage
 # Create your views here.
 
 
 def index(request):
-    return render(request, 'listings/listings.html')
+
+    listing = Listing.objects.order_by('-list_date').filter(is_published=True)
+
+    paginator = Paginator(listing,6)
+    page = request.GET.get('page')
+    paged_listings = paginator.get_page(page)
+    context = {
+        'listing': paged_listings
+    }
+
+    return render(request, 'listings/listings.html', context )
 
  
-def listing(request):
-    return render(request, template_name='listings/listing.html')
+def listing(request, listing_id):
+
+    listing = get_object_or_404(Listing, pk = listing_id)
+    context = {
+        'listing' : listing
+    }
+    return render(request, template_name='listings/listing.html', context=context)
 
 def search(request):
     return render(request, template_name='listings/search.html')
--- pages/views.py
+++ pages/views.py
@@ -1,12 +1,26 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from listings.models import Listing
+from realtors.models import Realtor
+
 
 # Create your views here.
 
 
 def index(request):
-    return render(request, 'pages/index.html')
+
+    listings = Listing.objects.order_by('-list_date').filter(is_published = True)[:3]
+    context = {
+        'listings': listings
+    }
+    return render(request, 'pages/index.html', context)
 
 
 def about(request):
-    return render(request, template_name='pages/about.html')
+    realtor = Realtor.objects.order_by('-hire_date')
+    mvp = Realtor.objects.filter(is_mvp= True )
+    context = {
+        'realtor': realtor,
+        'mvp': mvp
+    }
+    return render(request, template_name='pages/about.html',context=context)
