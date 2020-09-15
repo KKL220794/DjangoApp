a00648da86f064409d2edb56172369a4a951372e
krishna lal <krishna.lal@synechron.com>
2020-09-15 21:57:07 +0530

    dashboard made dynamic

--- btre/settings.py
+++ btre/settings.py
@@ -143,3 +143,11 @@ from django.contrib.messages import constants as messages
 MESSAGE_TAGS = {
     messages.ERROR: 'danger',
 }
+
+# emailers config
+
+EMAIL_HOST ='smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'test@email.com'
+EMAIL_HOST_PASSWORD = '123123'
+EMAIL_USE_TLS = True
--- contacts/views.py
+++ contacts/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from .models import Contact
+from django.core.mail import send_mail
 # Create your views here.
 
 
@@ -16,6 +17,21 @@ def contact(request):
         realtor_email = request.POST['realtor_email']
         contact = Contact(listing = listing, listing_id=listing_id, name=name, email=email, phone=phone, message=message, user_id=user_id)
 
+        # check if user has made entry
+        if request.user.is_authenticated:
+            user_id = request.POST['user_id']
+            has_contacted = Contact.objects.all().filter(listing_id=listing_id, user_id=user_id)
+            if has_contacted:
+                messages.error(request, 'You have already made a enquiry for this listing')
+                return redirect('/listings/' + listing_id)
+                
         contact.save()
+        # send_mail(
+        #             'Property listing enquiry',
+        #             'There has been a inquiry for ' + listing + '. sign in into the admin panel for info ' ,
+        #             'test@gmail.com',
+        #             [realtor_email, 'test@email.com'],
+        #             fail_silently=False,
+        #         )
         messages.success(request, 'Request submitted. Team will get back to you shortly')
         return redirect('/listings/' + listing_id)
